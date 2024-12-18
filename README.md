a8301bcaa2dd3c1935b129c3cd3e0f174f238a1b
michaelPotter <mpotter.dev@gmail.com>
2024-12-18 10:46:21 -0800

    mise-env-from-cmd

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
+# mise-env-from-cmd
+
+A mise plugin to set environment variables from shell commands.
+
 ## Usage
 
-Install this plugin, then add this to your `mise.toml`:
+Install this plugin
+
+```bash
+mise install mise-env-from-cmd https://github.com/michaelPotter/mise-env-from-cmd
+```
+
+Then update your `mise.toml`:
 
 ```toml
 [env]
-_.mise-env-sample = { path_to_add = "/sample/path" }
+_.mise-env-from-cmd.AWS_PROFILE = "jq -r .project.aws_profile .project_metadata.json"
+_.mise-env-from-cmd.MODULE = "jq -r .project.module .project_metadata.json"
 ```
 
-You'll now see this added to your environment when running `mise env`.
+You'll now see these added to your environment when running `mise env`.
--- hooks/mise_env.lua
+++ hooks/mise_env.lua
@@ -1,9 +1,25 @@
+--- @param name string
+--- @return string|nil
+local function run_cmd(cmd)
+    local handle, errmsg = io.popen("sh -c '" .. cmd .. "'")
+    local data = handle:read("*a")
+    suc, exitcode, code = handle:close()
+
+    return data
+end
+
 function PLUGIN:MiseEnv(ctx)
-    return {
-        {
-            key = "MISE_SAMPLE_ENV_KEY",
-            value = "foobar"
-        }
-    }
+
+    local env_vars = {}
+
+    for env_var_name, cmd in pairs(ctx.options) do
+        local result = run_cmd(cmd)
+        if result == nil then
+            error("Could not run command [" .. cmd .. "]")
+        end
+        table.insert(env_vars, {key = env_var_name, value = result})
+    end
+
+    return env_vars
 
 end
